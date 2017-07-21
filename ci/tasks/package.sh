aa862e79d0a7322b0b6ee08d20d1cad4b3a4d4e8
rathod-jay <rathod.jay@gmail.com>
2017-07-20 19:15:21 -0600

    package.yml modified

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,19 @@
 
 set -e +x
 
-pushd attendee-service-source
+pushd pcf-attendee-service-code
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+jar_count=`find pcf-attendee-service-code/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+find pcf-attendee-service-code/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
 
 echo "Done packaging"
 exit 0
